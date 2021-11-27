d4e273d99d70d9bf0740dbbd463577e0bb56e8d4
juanfer2 <jfvilladiego3@gmail.com>
2021-11-27 14:30:25 -0500

    Config deploy

---
+++ src/modules/projects/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { List, Avatar } from 'antd';
+
+function ProjectsModule() {
+  const data = [
+    {
+      title: 'Ant Design Title 1',
+    },
+    {
+      title: 'Ant Design Title 2',
+    },
+    {
+      title: 'Ant Design Title 3',
+    },
+    {
+      title: 'Ant Design Title 4',
+    },
+  ];
+
+  return (
+    <div>
+      <List
+        itemLayout="horizontal"
+        dataSource={data}
+        renderItem={item => (
+          <List.Item>
+            <List.Item.Meta
+              avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
+              title={<a href="https://ant.design">{item.title}</a>}
+              description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+            />
+          </List.Item>
+        )}
+      />
+    </div>
+  )
+}
+
+export default ProjectsModule
--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,46 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import { List, Avatar } from 'antd';
 import { CopyBlock, dracula  } from "react-code-blocks";
+import ProjectsModule from '../../modules/projects';
 import QueriesModule from '../../modules/queries';
 
 function Home() {
-  const data = [
-    {
-      title: 'Ant Design Title 1',
-    },
-    {
-      title: 'Ant Design Title 2',
-    },
-    {
-      title: 'Ant Design Title 3',
-    },
-    {
-      title: 'Ant Design Title 4',
-    },
-  ];
-
-  const code = 'import {  } from "react"'
-
-
-  // <List
-  //       itemLayout="horizontal"
-  //       dataSource={data}
-  //       renderItem={item => (
-  //         <List.Item>
-  //           <List.Item.Meta
-  //             avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
-  //             title={<a href="https://ant.design">{item.title}</a>}
-  //             description="Ant Design, a design language for background applications, is refined by Ant UED Team"
-  //           />
-  //         </List.Item>
-  //       )}
-  //     />
-
-
   return (
     <div>
-      <h1>HOME Rsjx</h1>
-      <QueriesModule/>
+      <h1>Home</h1>
+      <ProjectsModule/>
+      {/** <QueriesModule/> */}
     </div>
   )
 }
