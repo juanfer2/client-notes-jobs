a7255df73971119977d5b120b19b19634ea1d103
juanfer2 <64051193+juanfer2@users.noreply.github.com>
2021-12-07 19:47:51 -0500

    Update graphql.service.ts

--- src/services/graphqlService/graphql.service.ts
+++ src/services/graphqlService/graphql.service.ts
@@ -2,9 +2,10 @@ import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 import {env} from '../../contants/api.constant'
 
+// 'http://localhost:4000/graphql'
 // uri: env.apiUrlGraphql,
 const httpLink = createHttpLink({
-  uri: 'http://localhost:4000/graphql',
+  uri: env.apiUrlGraphql,
 });
 
 const authLink: any = setContext((_, { headers }) => {
